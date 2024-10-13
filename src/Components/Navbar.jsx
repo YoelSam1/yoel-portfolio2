154ab275c7aca643dad19c3836d3665285fa4309
YoelSam1 <yoelsamsom21@gmail.com>
2024-10-13 11:09:05 +0200

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,43 +2,37 @@ import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
 const NavbarComponent = ({ setActiveSection, activeSection }) => {
-  const [expanded, setExpanded] = useState(false); // State to control navbar collapse
+  const [expanded, setExpanded] = useState(false); 
 
   const handleToggle = () => setExpanded(!expanded);
-  const handleSelect = () => setExpanded(false); // Collapse navbar when a link is clicked
+  const handleSelect = () => setExpanded(false); 
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top" expanded={expanded}>
       <Navbar.Toggle 
         aria-controls="basic-navbar-nav" 
         className="ms-auto me-5" 
-        onClick={handleToggle} // Toggle the expanded state
+        onClick={handleToggle} 
       />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="w-100 justify-content-center text-center">
           <Nav.Link
             onClick={() => { setActiveSection("about"); handleSelect(); }}
-            className={`mx-2 ${activeSection === "about" ? 'text-white font-weight-bold' : 'text-secondary'}`} // Darker when active, lighter when inactive
+            className={`mx-2 ${activeSection === "about" ? 'text-white font-weight-bold' : 'text-secondary'}`} 
           >
             About
           </Nav.Link>
           <Nav.Link
             onClick={() => { setActiveSection("projects"); handleSelect(); }}
-            className={`mx-2 ${activeSection === "projects" ? 'text-white font-weight-bold' : 'text-secondary'}`} // Darker when active, lighter when inactive
+            className={`mx-2 ${activeSection === "projects" ? 'text-white font-weight-bold' : 'text-secondary'}`} 
           >
             Projects
           </Nav.Link>
           <Nav.Link
             onClick={() => { setActiveSection("skills"); handleSelect(); }}
-            className={`mx-2 ${activeSection === "skills" ? 'text-white font-weight-bold' : 'text-secondary'}`} // Darker when active, lighter when inactive
+            className={`mx-2 ${activeSection === "skills" ? 'text-white font-weight-bold' : 'text-secondary'}`} 
           >
             Skills
-          </Nav.Link>
-          <Nav.Link
-            onClick={() => { setActiveSection("contact"); handleSelect(); }}
-            className={`mx-2 ${activeSection === "contact" ? 'text-white font-weight-bold' : 'text-secondary'}`} // Darker when active, lighter when inactive
-          >
-            Contact
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Card, Col, Row, Button } from 'react-bootstrap';
 import NavigationBar from '../Components/Navbar';
+import '../Assets/custom/custom.css'; 
 
 // Import images
 import jsFrameworksCA from '../Assets/images/JavaScript-Frameworks-CA.png';
@@ -10,21 +11,21 @@ import semesterProject2 from '../Assets/images/Semester-Project-2.png';
 const projects = [
   {
     title: "Semester Project 2",
-    description: "This is a description of Semester Project 2.",
+    description: "New users receive 1000 credits upon joining, which can be earned by selling items and spent on purchases, while only registered users with a stud.noroff.no email can register, log in, update their avatar, view total credits, create listings, and place bids on other listings.",
     imageUrl: semesterProject2,
     githubUrl: "https://github.com/YoelSam1/Semester-Project-2",
     liveUrl: "https://semester-project-2-yoel.netlify.app/",
   },
   {
     title: "JavaScript Frameworks CA",
-    description: "A React-based eCommerce store where users can browse products, add items to a cart, and complete purchases through a checkout process. The app features a searchable product list, individual product pages, and a validated contact form. It integrates with a RESTful API for product data and transactions.",
+    description: "A React eCommerce store where users can browse products, add items to a cart, and complete purchases. It includes a searchable product list, individual product pages, and a validated contact form, all integrated with a RESTful API for product data and transactions.",
     imageUrl: jsFrameworksCA,
     githubUrl: "https://github.com/YoelSam1/Front-end-React-E-com",
     liveUrl: "https://react-ca-yoel.netlify.app/",
   },
   {
     title: "Project Exam 2",
-    description: "This is a description of Project Exam 2.",
+    description: "The Holidaze project develops a modern front end for an accommodation booking site, highlighting skills from two years in Front-End Development. It features venue search, availability checking, booking, and venue management, all integrated with the Noroff API.",
     imageUrl: projectExam2,
     githubUrl: "https://github.com/YoelSam1/Yoel-projectexam2",
     liveUrl: "https://funny-sprinkles-8fcd40.netlify.app/",
@@ -35,7 +36,6 @@ const Portfolio = () => {
   const [activeSection, setActiveSection] = useState(window.location.hash.replace('#', '') || 'about');
 
   useEffect(() => {
-    // Update URL when the active section changes
     window.location.hash = activeSection;
   }, [activeSection]);
 
@@ -43,18 +43,30 @@ const Portfolio = () => {
     switch (activeSection) {
       case "about":
         return (
-          <section id="about" className="bg-warning text-dark py-5 d-flex justify-content-center align-items-center flex-column" style={{ minHeight: '100vh' }}>
-            <div className="container text-center">
+          <section
+            id="about"
+            className="bg-secondary text-white py-5 d-flex justify-content-center align-items-center flex-column full-height"
+          >
+            <div className="container text-center p-4 shadow-lg custom-background">
               <h1 className="mb-5 mt-5">About Me</h1>
               <p className="lead">I am a passionate developer with experience in building dynamic web applications.</p>
-              <p>My journey in technology started when I was curious about how websites work...</p>
+              <p className="lead">My journey in technology started when I was curious about how websites work. Since then, I have dedicated myself to learning and growing as a developer.</p>
+              <p className="lead">
+                I have been a student at Noroff School of Technology and Digital Media for the past two years, where I have honed my skills in front-end and back-end development, focusing on modern technologies like React, JavaScript, and REST APIs.
+              </p>
+              <p className="lead">
+                Throughout my time at Noroff, I have worked on a variety of challenging projects that have helped me develop problem-solving skills and a deep understanding of web development.
+              </p>
             </div>
           </section>
         );
 
       case "projects":
         return (
-          <section id="projects" className="bg-primary text-light py-5 d-flex justify-content-center align-items-center flex-column" style={{ minHeight: '100vh' }}>
+          <section
+            id="projects"
+            className="bg-primary text-light py-5 d-flex justify-content-center align-items-center flex-column full-height"
+          >
             <div className="container text-center">
               <h1 className="mb-5 mt-5">My Projects</h1>
               <Row>
@@ -66,10 +78,19 @@ const Portfolio = () => {
                         <Card.Title className="mb-3">{project.title}</Card.Title>
                         <Card.Text className="mb-4">{project.description}</Card.Text>
                         <div className="d-flex justify-content-center">
-                          <Button href={project.githubUrl} target="_blank" className="me-2" variant="outline-primary">
+                          <Button
+                            href={project.githubUrl}
+                            target="_blank"
+                            className="me-2"
+                            variant="outline-primary"
+                          >
                             GitHub Repo
                           </Button>
-                          <Button href={project.liveUrl} target="_blank" variant="outline-success">
+                          <Button
+                            href={project.liveUrl}
+                            target="_blank"
+                            variant="outline-success"
+                          >
                             Live Site
                           </Button>
                         </div>
@@ -84,35 +105,26 @@ const Portfolio = () => {
 
       case "skills":
         return (
-          <section id="skills" className="bg-info text-white py-5 d-flex justify-content-center align-items-center flex-column" style={{ minHeight: '100vh' }}>
-            <div className="container text-center">
+          <section
+            id="skills"
+            className="bg-info text-white py-5 d-flex justify-content-center align-items-center flex-column full-height"
+          >
+            <div className="container text-center p-4 shadow-lg">
               <h1 className="mb-5 mt-5">Skills</h1>
               <Row>
                 <Col md={4}>
-                  <h5>Front-End Development</h5>
+                  <h5 >Front-End Development</h5>
                   <p>HTML, CSS, JavaScript, React, Bootstrap</p>
                 </Col>
                 <Col md={4}>
                   <h5>Back-End Development</h5>
-                  <p>Node.js, Express, MongoDB, REST APIs</p>
+                  <p>Node.js, REST APIs</p>
                 </Col>
                 <Col md={4}>
                   <h5>Other Skills</h5>
-                  <p>Git, Agile Methodologies, UI/UX Design</p>
+                  <p>Git, UI/UX Design</p>
                 </Col>
               </Row>
-            </div>
-          </section>
-        );
-
-      case "contact":
-        return (
-          <section id="contact" className="bg-secondary text-white py-5 d-flex justify-content-center align-items-center flex-column" style={{ minHeight: '100vh' }}>
-            <div className="container text-center">
-              <h1 className="mb-5 mt-5">Contact</h1>
-              <p>If you'd like to get in touch, feel free to reach out!</p>
-              <p>Email: <a href="mailto:your.email@example.com" className="text-light">your.email@example.com</a></p>
-              <p>LinkedIn: <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className="text-light">linkedin.com/in/yourprofile</a></p>
             </div>
           </section>
         );
